26a76404464ba456b76ceb11f08076b7ba4f5a84
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2020-01-29 16:49:16 +0700

    [Interop] Generate IR for CEnum inheritors
    Since there is non-trivial logic inside code that we generate for interop enums, we generate it inside special IrProvider.

---
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/ir/interop/cenum/CEnumVarClassGenerator.kt
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the LICENSE file.
+ */
+package org.jetbrains.kotlin.backend.konan.ir.interop.cenum
+
+import org.jetbrains.kotlin.backend.konan.InteropBuiltIns
+import org.jetbrains.kotlin.backend.konan.descriptors.getArgumentValueOrNull
+import org.jetbrains.kotlin.backend.konan.ir.interop.DescriptorToIrTranslationMixin
+import org.jetbrains.kotlin.descriptors.ClassDescriptor
+import org.jetbrains.kotlin.incremental.components.NoLookupLocation
+import org.jetbrains.kotlin.ir.builders.irBlockBody
+import org.jetbrains.kotlin.ir.builders.irGet
+import org.jetbrains.kotlin.ir.builders.irInt
+import org.jetbrains.kotlin.ir.declarations.IrClass
+import org.jetbrains.kotlin.ir.declarations.IrConstructor
+import org.jetbrains.kotlin.ir.declarations.IrProperty
+import org.jetbrains.kotlin.ir.declarations.addMember
+import org.jetbrains.kotlin.ir.descriptors.IrBuiltIns
+import org.jetbrains.kotlin.ir.expressions.impl.IrDelegatingConstructorCallImpl
+import org.jetbrains.kotlin.ir.expressions.impl.IrInstanceInitializerCallImpl
+import org.jetbrains.kotlin.ir.util.*
+import org.jetbrains.kotlin.ir.util.irBuilder
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.Name
+
+private val typeSizeAnnotation = FqName("kotlinx.cinterop.internal.CEnumVarTypeSize")
+
+internal class CEnumVarClassGenerator(
+        override val irBuiltIns: IrBuiltIns,
+        override val symbolTable: SymbolTable,
+        override val typeTranslator: TypeTranslator,
+        private val interopBuiltIns: InteropBuiltIns
+) : DescriptorToIrTranslationMixin {
+    fun generate(enumIrClass: IrClass): IrClass {
+        val enumVarClassDescriptor = enumIrClass.descriptor.unsubstitutedMemberScope
+                .getContributedClassifier(Name.identifier("Var"), NoLookupLocation.FROM_BACKEND)!! as ClassDescriptor
+        return createClass(enumVarClassDescriptor) { enumVarClass ->
+            enumVarClass.addMember(createPrimaryConstructor(enumVarClass))
+            enumVarClass.addMember(createCompanionObject(enumVarClass))
+            enumVarClass.addMember(createValueProperty(enumVarClass))
+        }
+    }
+
+    private fun createValueProperty(enumVarClass: IrClass): IrProperty {
+        val valuePropertyDescriptor = enumVarClass.descriptor.unsubstitutedMemberScope
+                .getContributedVariables(Name.identifier("value"), NoLookupLocation.FROM_BACKEND).single()
+        val irValueProperty = createProperty(valuePropertyDescriptor)
+        symbolTable.withScope(valuePropertyDescriptor) {
+            irValueProperty.getter = declareSimpleIrFunction(valuePropertyDescriptor.getter!!).also { getter ->
+                getter.correspondingPropertySymbol = irValueProperty.symbol
+            }
+            irValueProperty.setter = declareSimpleIrFunction(valuePropertyDescriptor.setter!!).also { setter ->
+                setter.correspondingPropertySymbol = irValueProperty.symbol
+            }
+        }
+        return irValueProperty
+    }
+
+    private fun createPrimaryConstructor(enumVarClass: IrClass): IrConstructor {
+        val irConstructor = createConstructor(enumVarClass.descriptor.unsubstitutedPrimaryConstructor!!)
+        val enumVarConstructorSymbol = symbolTable.referenceConstructor(
+                interopBuiltIns.cEnumVar.unsubstitutedPrimaryConstructor!!
+        )
+        irConstructor.body = irBuilder(irBuiltIns, irConstructor.symbol).irBlockBody {
+            +IrDelegatingConstructorCallImpl(
+                    SYNTHETIC_OFFSET, SYNTHETIC_OFFSET, context.irBuiltIns.unitType, enumVarConstructorSymbol
+            ).also {
+                it.putValueArgument(0, irGet(irConstructor.valueParameters[0]))
+            }
+            +IrInstanceInitializerCallImpl(
+                    SYNTHETIC_OFFSET, SYNTHETIC_OFFSET,
+                    symbolTable.referenceClass(enumVarClass.descriptor),
+                    context.irBuiltIns.unitType
+            )
+        }
+        return irConstructor
+    }
+
+    private fun createCompanionObject(enumVarClass: IrClass): IrClass =
+            createClass(enumVarClass.descriptor.companionObjectDescriptor!!) { companionIrClass ->
+                val typeSize = companionIrClass.descriptor.annotations
+                        .findAnnotation(typeSizeAnnotation)!!
+                        .getArgumentValueOrNull<Int>("size")!!
+                companionIrClass.addMember(createCompanionConstructor(companionIrClass.descriptor, typeSize))
+            }
+
+    private fun createCompanionConstructor(companionObjectDescriptor: ClassDescriptor, typeSize: Int): IrConstructor {
+        val irConstructor = createConstructor(companionObjectDescriptor.unsubstitutedPrimaryConstructor!!)
+        val superConstructorSymbol = symbolTable.referenceConstructor(interopBuiltIns.cPrimitiveVarType.unsubstitutedPrimaryConstructor!!)
+        irConstructor.body = irBuilder(irBuiltIns, irConstructor.symbol).irBlockBody {
+            +IrDelegatingConstructorCallImpl(
+                    SYNTHETIC_OFFSET, SYNTHETIC_OFFSET, context.irBuiltIns.unitType,
+                    superConstructorSymbol
+            ).also {
+                it.putValueArgument(0, irInt(typeSize))
+            }
+            +IrInstanceInitializerCallImpl(
+                    SYNTHETIC_OFFSET, SYNTHETIC_OFFSET,
+                    symbolTable.referenceClass(companionObjectDescriptor),
+                    context.irBuiltIns.unitType
+            )
+        }
+        return irConstructor
+    }
+}
